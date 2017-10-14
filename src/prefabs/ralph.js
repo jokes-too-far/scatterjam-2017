702439fecf7f6e5b109ef9ff77bff6b6bd15c768
Evan <evan@projectpixelpress.com>
2017-10-14 14:35:40 -0500

    also hand Ralph over to endLevel if you lose

--- src/prefabs/ralph.js
+++ src/prefabs/ralph.js
@@ -36,7 +36,7 @@ class Ralph extends Phaser.Sprite {
     //play animation of bro eating candy
     const timer = this.game.time.create(false)
     timer.add(Phaser.Timer.SECOND * 1.5, () => {
-      this.game.state.start('endLevel', false)
+      this.game.state.start('endLevel', false, true, this)
     })
     timer.start();
   }
