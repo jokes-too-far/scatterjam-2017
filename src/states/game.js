640847deb4dee930b1e5ef4157fdbd13813c3392
Evan <evan@projectpixelpress.com>
2017-10-13 22:50:21 -0500

    timer naively moves to you to next state (always lose)

--- src/states/game.js
+++ src/states/game.js
@@ -9,13 +9,10 @@ class Game extends Phaser.State {
   create() {
     const timer = this.game.time.create(false)
     timer.add(Phaser.Timer.SECOND * 5, () => {
-      console.log('done');
+      this.endGame()
     })
     timer.start()
     new TimerDisplay(this.game, timer)
-    console.log(timer);
-
-    this.input.onDown.add(this.endGame, this)
   }
 
   update() {
