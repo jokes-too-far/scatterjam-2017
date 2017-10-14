acb98b932e7320356ad1f718d6f185104c7c4c58
Evan <evan@projectpixelpress.com>
2017-10-13 22:55:53 -0500

    rounding the timer better

--- src/prefabs/timerDisplay.js
+++ src/prefabs/timerDisplay.js
@@ -1,15 +1,18 @@
 import style from '../fontStyle'
 
+const decimalPlacesToShow = 1
+
 class TimerDisplay extends Phaser.Text {
 
   constructor(game, timer) {
-    super(game, game.centerX, game.centerY, 'default text yo', style)
+    super(game, game.world.centerX, 0, '', style)
     this.timer = timer
+    this.anchor.setTo(0.5, 0)
     game.add.existing(this)
   }
 
   update() {
-    this.text = this.timer.duration / Phaser.Timer.SECOND
+    this.text = (this.timer.duration / Phaser.Timer.SECOND).toFixed(decimalPlacesToShow)
   }
 
 }
