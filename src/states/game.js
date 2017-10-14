0273250c1d8125812492ffec5617b81cf6165029
Sam Jantz <sjantz0@gmail.com>
2017-10-13 22:53:25 -0500

    actually merging

--- src/states/game.js
+++ src/states/game.js
@@ -8,22 +8,13 @@ class Game extends Phaser.State {
   }
 
   create() {
-<<<<<<< HEAD
-    var text = this.add.text(this.game.width * 0.5, this.game.height * 0.5, 'Game', {
-      font: '42px Arial', fill: '#ffffff', align: 'center'
-    });
-    text.anchor.set(0.5);
-    new Gordon();
-
-    this.input.onDown.add(this.endGame, this);
-=======
     const timer = this.game.time.create(false)
     timer.add(Phaser.Timer.SECOND * 5, () => {
       this.endGame()
     })
     timer.start()
     new TimerDisplay(this.game, timer)
->>>>>>> 640847deb4dee930b1e5ef4157fdbd13813c3392
+    new Gordon();
   }
 
   update() {
