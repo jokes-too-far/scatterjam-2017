4c196f9dc5e3595eabf33d2017b80f24323899ae
Becky Asbury <rasbury7@gmail.com>
2017-10-13 22:52:24 -0500

    added splash screen

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Game from './states/game';
 import Menu from './states/menu';
 import Preloader from './states/preloader';
 import Gameover from './states/gameover';
+const SplashScreen = require('./states/splashScreen');
 
 
 const game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'scatterjam-2017-game');
@@ -12,5 +13,6 @@ game.state.add('game', new Game());
 game.state.add('menu', new Menu());
 game.state.add('preloader', new Preloader());
 game.state.add('gameover', new Gameover());
+game.state.add('splashScreen', new SplashScreen());
 
 game.state.start('boot');
