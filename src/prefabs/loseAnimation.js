0ca6a9db265cd3862950d6583284054efc1d7b4e
Evan <evan@projectpixelpress.com>
2017-10-15 14:47:24 -0500

    animating the lose screen

--- src/prefabs/loseAnimation.js
+++ src/prefabs/loseAnimation.js
@@ -2,7 +2,8 @@ class LoseAnimation extends Phaser.Sprite {
 
   constructor(game) {
     super(game, game.world.centerX, game.world.centerY, 'lose-screen')
-    // TODO animation fun
+    this.animations.add('play', [0,1,2,3], 2, false);
+    this.animations.play('play');
     this.width = 1024
     this.height = 1024
     this.bringToTop()
