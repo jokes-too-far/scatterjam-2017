d6160837b108ad1ab3864d4a07dd0a27f588c0a7
Evan <evan@projectpixelpress.com>
2017-10-14 22:22:11 -0500

    destroying triggers events, kill just removes the graphic

--- src/prefabs/textBackground.js
+++ src/prefabs/textBackground.js
@@ -1,6 +1,6 @@
 class TextBackground extends Phaser.TileSprite {
 
-  constructor(game, y, width, height) {
+  constructor(game, y) {
     super(game, 0, y, 0, 256, 'textBackground')
     this.scale.y = 0.5
     this.anchor.setTo(0, 0.5)
