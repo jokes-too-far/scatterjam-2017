e10407bba7048df7b8b91b7f15971b9f79b80c28
Becky Asbury <rasbury7@gmail.com>
2017-10-14 23:09:23 -0500

    Ralph walks

--- src/prefabs/ralph.js
+++ src/prefabs/ralph.js
@@ -9,11 +9,13 @@ class Ralph extends Phaser.Sprite {
     y = y - (SPRITE_SIZE / 2)
     super(game, x, y, 'ralph', frame);
     this.anchor.setTo(.5);
+    this.animations.add('run', [1,2,3,4,5,6,7,8], 3, true);
     console.log("I AM RALPH, I AM MEAN")
     game.physics.enable(this, Phaser.Physics.ARCADE);
     this.body.setSize(50, 128, 40, 0);
     this.body.velocity.x = 0
     game.add.existing(this);
+    this.animations.play("run");
   }
 
   update() {
